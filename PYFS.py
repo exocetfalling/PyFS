5331c0bd83d536c72039a7c2744f1fa31fc254f4
toffee2000 <toffee2000@gmail.com>
2021-08-05 10:23:16 +0800

    Adds lift calc

--- PYFS.py
+++ PYFS.py
@@ -1,5 +1,7 @@
 # Imports
+from re import A
 import keyboard  # using module keyboard
+import numpy as np
 from datetime import datetime
 import os
 
@@ -18,41 +20,28 @@ a_beta = 0
 a_cl = 0
 a_cd = 0
 a_dynp = 0
-a_air_density = 0
-a_airspeed_true = 0
+a_air_density = 1.3
+a_airspeed_true = 200
 
 a_lift_force = 0
 a_drag_force = 0
 
-a_lift_vector = [0, 0, 0]
-a_drag_vector = [0, 0, 0]
-a_thrust_vector = [0, 0 ,0]
-a_total_vector = [0, 0, 0]
+a_lift_vector = np.array([0, 0, 0])
+a_drag_vector = np.array([0, 0, 0])
+a_thrust_vector = np.array([0, 0 ,0])
+a_weight_vector = np.array([0, 0, 0])
+a_total_vector = np.array([0, 0, 0])
 
-w_weight_vector = [0, 0, 0]
-w_total_vector = [0, 0, 0]
+w_total_vector = np.array([0, 0, 0])
 
 a_elevator_pct = 0
 a_aileron_pct = 0
 a_rudder_pct = 0
 
-c_wing_area = 0
+c_wing_area = 500
 c_elevator_area = 0
 c_aileron_area = 0
 
-# Functions
-
-def calc_lift_force(a_air_density, a_airspeed_true, c_wing_area, a_cl):
-    a_lift_force = (0.5 * a_air_density * a_airspeed_true * a_airspeed_true * c_wing_area * a_cl)
-    return a_lift_force
-
-def calc_cl(a_alpha):
-    a_cl = -0.007 * (a_alpha - 15) * (a_alpha - 15) + 1.7
-    return a_cl
-
-def calc_lift_vector(a_lift_force):
-    a_lift_vector = [0, a_lift_force, 0]
-    return a_lift_vector
 
 # Main loop
 while True:  # making a loop
@@ -61,15 +50,24 @@ while True:  # making a loop
     now  = datetime.now()                         # Now
     delta_time = (now - then).total_seconds()      # Total number of seconds between now and then
     #print(delta_time)
-    print('Pitch:', round(a_pitch))
+
+    a_lift_force = (0.5 * a_air_density * a_airspeed_true * a_airspeed_true * c_wing_area * a_cl)
+    a_cl = -0.007 * (a_alpha - 15) * (a_alpha - 15) + 1.7
+    a_lift_vector = ([0, a_lift_force, 0])
+    a_total_vector = [a_lift_vector] + [a_drag_vector] + [a_thrust_vector] + [a_weight_vector]
+
+    print('Alpha:', round(a_alpha))
     print('Roll:', round(a_roll))
+    print(a_total_vector)
+    print(a_lift_force)
+    print(a_cl)
     os.system('cls' if os.name == 'nt' else 'clear')
 
     
     #add control code here
     try:  # used try so that if user pressed other than the given key error will not be shown
         if keyboard.is_pressed('w'):  # if key 'w' is pressed 
-            a_pitch = a_pitch - 1
+            a_alpha = a_alpha - 1
             #print('You Pressed A Key!')
             #break  # finishing the loop
 
@@ -81,7 +79,7 @@ while True:  # making a loop
         if keyboard.is_pressed('s'):  # if key 's' is pressed 
             #print('You Pressed A Key!')
             #break  # finishing the loop
-            a_pitch = a_pitch + 1
+            a_alpha = a_alpha + 1
 
         if keyboard.is_pressed('d'):  # if key 'd' is pressed 
             #print('You Pressed A Key!')
