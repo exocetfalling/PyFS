07c2c68a9affb77303b4026c9ea11101ec677ffa
toffee2000 <70193215+toffee2000@users.noreply.github.com>
2021-08-08 12:09:46 +0800

    Update PYFS.py

--- PYFS.py
+++ PYFS.py
@@ -9,15 +9,23 @@ import pygame
 import pygame.freetype  # Import the freetype module.
 
 # Variables
-a_fpa_deg = 0
-a_fpa_rad = 0
-a_roll_deg = 0
-a_roll_rad = 0
+
+# Polar coordinates as as commonly used in physics (ISO 80000-2:2019 convention): 
+# radial distance r (distance to origin), 
+# polar angle theta (angle with respect to polar axis), 
+# azimuthal angle phi (angle of rotation from the initial meridian plane).
+
+a_theta_deg = 0
+a_theta_rad = 0
+a_phi_deg = 0
+a_phi_rad = 0
+a_radial_velocity = 50
 
 a_airspeed_indicated = 0
 a_alt = 0
-a_trk_rad = 0
-a_trk_deg = 0
+
+a_roll_deg = 0
+a_roll_rad = 0
 
 a_alpha = 0
 a_beta = 0
@@ -27,7 +35,7 @@ a_cd = 0
 a_dynp = 0
 a_air_density = 1.3
 a_airspeed_true = 50
-a_gnd_speed = 50
+
 a_lift_force = 0
 a_drag_force = 0
 
@@ -67,13 +75,13 @@ screen = pygame.display.set_mode(SIZE, pygame.RESIZABLE)
 clock = pygame.time.Clock()
 
 # Functions
-def Calc_Velocity_World(axis, total_vel, angle_horizontal, angle_vertical): 
+def Calc_Velocity_World(axis, total_vel, angle_azimuthal, angle_polar): 
     if axis == 'x':
-        return (total_vel * math.sin(angle_horizontal)) * math.cos(angle_vertical)
+        return (total_vel * math.sin(angle_azimuthal)) * math.sin(angle_polar)
     if axis == 'y':
-        return (total_vel * math.cos(angle_horizontal)) * math.cos(angle_vertical)
+        return (total_vel * math.cos(angle_azimuthal)) * math.sin(angle_polar)
     if axis == 'z':
-        return (total_vel * math.sin(angle_vertical)) * math.cos(angle_horizontal)
+        return (total_vel * math.cos(angle_polar)) * math.cos(angle_azimuthal)
 
 def Calc_Force_Moment(force_magnitude, distance_from_pivot):
     return force_magnitude * distance_from_pivot
@@ -81,11 +89,22 @@ def Calc_Force_Moment(force_magnitude, distance_from_pivot):
 def Calc_Force_Acc(force_magnitude, mass_kg):
     return force_magnitude / mass_kg
 
-def Calc_Lift_Coeff(angle_rad):
-    return -0.007 * (angle_rad - 15) * (angle_rad - 15) + 1.7
+def Calc_Lift_Coeff(angle_alpha_rad):
+    if ((angle_alpha_rad > (-math.pi)) and (angle_alpha_rad <= (-math.pi/12))):
+        pass
+    if ((angle_alpha_rad > (-math.pi/12)) and (angle_alpha_rad <= (math.pi/12))):
+        pass
+    if ((angle_alpha_rad > (math.pi/12)) and (angle_alpha_rad <= (math.pi))):
+        pass
 
 def Calc_Drag_Coeff(angle_rad):
     return math.sin(angle_rad)
+
+def Convert_Angle_Rad_To_Deg(angle_rad):
+    return angle_rad * 57.2958
+
+def Convert_Angle_Deg_To_Rad(angle_deg):
+    return angle_deg / 57.2958
 
 def blit_text(surface, text, pos, font, color=pygame.Color('black')):
     words = [word.split(' ') for word in text.splitlines()]  # 2D array where each row is a list of words.
@@ -115,12 +134,13 @@ while True:
         '\nX Pos: ' + str(w_x_pos) + \
         '\nY Pos: ' + str(w_y_pos) + \
         '\nZ Pos: ' + str(w_z_pos) + \
-        '\nFPA: ' + str(a_fpa_deg) + \
-        '\nHDG: ' + str(a_trk_deg)
-    a_trk_deg = (a_trk_deg + 360) % 360
-    a_fpa_deg = (a_fpa_deg + 180) % 180
-    a_trk_rad = a_trk_deg / 57.2958
-    a_fpa_rad = a_fpa_deg / 57.2958
+        '\nTHETA: ' + str(a_theta_deg) + \
+        '\nPHI: ' + str(a_phi_deg)
+    a_phi_deg = (a_phi_deg + 360) % 360
+    a_theta_deg = (a_theta_deg + 360) % 360
+
+    a_phi_rad = Convert_Angle_Deg_To_Rad(a_phi_deg)
+    a_theta_rad = Convert_Angle_Deg_To_Rad(a_theta_deg)
     a_lift_force = (0.5 * a_air_density * a_airspeed_true * a_airspeed_true * c_wing_area * a_cl)
     a_cl = Calc_Lift_Coeff(a_alpha)
     
@@ -130,9 +150,9 @@ while True:
     w_z_velocity = a_gnd_speed * math.sin(a_fpa_rad)
 
     """
-    w_x_velocity = Calc_Velocity_World('x', a_gnd_speed, a_trk_rad, a_fpa_rad)
-    w_y_velocity = Calc_Velocity_World('y', a_gnd_speed, a_trk_rad, a_fpa_rad)
-    w_z_velocity = Calc_Velocity_World('z', a_gnd_speed, a_roll_rad, a_fpa_rad)
+    w_x_velocity = Calc_Velocity_World('x', a_radial_velocity, a_phi_rad, a_theta_rad)
+    w_y_velocity = Calc_Velocity_World('y', a_radial_velocity, a_phi_rad, a_theta_rad)
+    w_z_velocity = Calc_Velocity_World('z', a_radial_velocity, a_roll_rad, a_theta_rad)
 
     w_x_pos = w_x_pos + w_x_velocity * dt
     w_y_pos = w_y_pos + w_y_velocity * dt
@@ -141,16 +161,16 @@ while True:
     keys=pygame.key.get_pressed()
 
     if keys[pygame.K_w]:
-        a_fpa_deg = a_fpa_deg - 10 * dt
+        a_theta_deg = a_theta_deg - 10 * dt
 
     if keys[pygame.K_s]:
-        a_fpa_deg = a_fpa_deg + 10 * dt
+        a_theta_deg = a_theta_deg + 10 * dt
 
     if keys[pygame.K_a]:
-        a_trk_deg = a_trk_deg - 10 * dt
+        a_phi_deg = a_phi_deg - 10 * dt
 
     if keys[pygame.K_d]:
-        a_trk_deg = a_trk_deg + 10 * dt
+        a_phi_deg = a_phi_deg + 10 * dt
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
